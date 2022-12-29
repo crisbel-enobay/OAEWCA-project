d6922e31c713403f93ab10c098f0f030b1e7c771
Crisbel-Enobay <91605851+Crisbel-Enobay@users.noreply.github.com>
2022-12-29 10:55:41 +0800

    user side - nav

---
+++ views/user-results.php
@@ -0,0 +1,228 @@
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+    <!-- Required meta tags -->
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <title>Admin Dashboard</title>
+    <!-- plugins:css -->
+    <link rel="stylesheet" href="../vendors/simple-line-icons/css/simple-line-icons.css">
+    <link rel="stylesheet" href="../vendors/flag-icon-css/css/flag-icon.min.css">
+    <link rel="stylesheet" href="../vendors/css/vendor.bundle.base.css">
+    <!-- endinject -->
+    <!-- Plugin css for this page -->
+    <link rel="stylesheet" href="../vendors/daterangepicker/daterangepicker.css">
+    <link rel="stylesheet" href="../vendors/chartist/chartist.min.css">
+    <!-- End plugin css for this page -->
+    <!-- inject:css -->
+    <!-- endinject -->
+    <!-- Layout styles -->
+    <link rel="stylesheet" href="../assets/css/style-admin.css">
+    <!-- End layout styles -->
+    <link rel="shortcut icon" href="../assets/img/ucc.png" />
+  </head>
+  <body>
+    <div class="container-scroller">
+      <!-- partial:partials/_navbar.html -->
+      <nav class="navbar default-layout-navbar col-lg-12 col-12 p-0 fixed-top d-flex flex-row">
+        <div class="navbar-brand-wrapper d-flex align-items-center">
+          <a class="navbar-brand brand-logo" href="../views/admin.php">
+            <img src="../assets/img/OAEWCA-LOGO copy.png" alt="logo" class="logo-dark" />
+          </a>
+          <a class="navbar-brand brand-logo-mini" href="../views/admin.php"><img src="../assets/img/OAEWCA-LOGO copy.png" alt="logo" /></a>
+        </div>
+        <div class="navbar-menu-wrapper d-flex align-items-center flex-grow-1">
+          <h5 class="mb-0 font-weight-medium d-none d-lg-flex">Welcome Student!</h5>
+          <ul class="navbar-nav navbar-nav-right ml-auto">
+            <li class="nav-item dropdown d-none d-xl-inline-flex user-dropdown">
+              <a class="nav-link dropdown-toggle" id="UserDropdown" href="#" data-toggle="dropdown" aria-expanded="false">
+               <span class="font-weight-normal"> Student user </span></a>
+              <div class="dropdown-menu dropdown-menu-right navbar-dropdown" aria-labelledby="UserDropdown">
+                <div class="dropdown-header text-center">
+                  <p class="mb-1 mt-3">Student user</p>
+                  <p class="font-weight-light text-muted mb-0">student@gmail.com</p>
+                </div>
+                <a class="dropdown-item"><i class="dropdown-item-icon icon-power text-primary"></i>Sign Out</a>
+              </div>
+            </li>
+          </ul>
+          <button class="navbar-toggler navbar-toggler-right d-lg-none align-self-center" type="button" data-toggle="offcanvas">
+            <span class="icon-menu"></span>
+          </button>
+        </div>
+      </nav>
+      <!-- partial -->
+      <div class="container-fluid page-body-wrapper">
+        <!-- partial:partials/_sidebar.html -->
+        <nav class="sidebar sidebar-offcanvas" id="sidebar">
+          <ul class="nav">
+            <li class="nav-item nav-profile">
+              <a href="#" class="nav-link">
+                <div class="text-wrapper">
+                  <p class="profile-name">Student user</p>
+                  <p class="designation">Student</p>
+                </div>
+                <div class="icon-container">
+                  <i class="icon-user"></i>
+                  <div class="dot-indicator bg-success"></div>
+                </div>
+              </a>
+            </li>
+            <li class="nav-item nav-category">
+              <span class="nav-link">Student Dashboard</span>
+            </li>
+            <li class="nav-item">
+              <a class="nav-link" href="../views/user-dashboard.php">
+                <span class="menu-title">Dashboard</span>
+                <i class="icon-screen-desktop menu-icon"></i>
+              </a>
+            </li>
+            <li class="nav-item nav-category"><span class="nav-link">Admission Exam</span></li>
+            <li class="nav-item">
+              <a class="nav-link" href="../views/user-exam.php">
+                <span class="menu-title">Examination</span>
+                <i class="icon-globe menu-icon"></i>
+              </a>
+            </li>
+            </li>
+            <li class="nav-item nav-category"><span class="nav-link">History</span></li>
+            <li class="nav-item">
+              <a class="nav-link" href="../views/user-results.php">
+                <span class="menu-title">Results</span>
+                <i class="icon-chart menu-icon"></i>
+              </a>
+            </li>
+          </ul>
+        </nav>
+       <!-- partial -->
+       <div class="main-panel">
+        <div class="content-wrapper">
+          <div class="page-header">
+            <nav>
+              <ol class="breadcrumb">
+                <li class="breadcrumb-item active">Results</li>
+            <li class="breadcrumb-item"><a href="../views/user-dashboard.php">Home</a></li>
+              </ol>
+            </nav>
+          </div>
+          <!-- Quick Action Toolbar Starts-->
+          <div class="row quick-action-toolbar">
+            <div class="col-md-12 grid-margin">
+              <div class="card">
+                <div class="card-body">
+                  <div class="card-header d-block d-md-flex">
+                    <h5 class="mb-0">Overall Result</h5>
+                  </div>
+                  <div class="table-responsive border rounded p-1">
+                    <table class="table">
+                      <thead>
+                        <tr>
+                          <th class="font-weight-bold">ID</th>
+                          <th class="font-weight-bold">Student Name</th>
+                          <th class="font-weight-bold">Course</th>
+                          <th class="font-weight-bold">Sample</th>
+                          <th class="font-weight-bold">Score</th>
+                          <th class="font-weight-bold">Status</th>
+                        </tr>
+                      </thead>
+                      <tbody>
+                        <tr>
+                          <td>
+                            1
+                          </td>
+                          <td>Crisbel Enobay</td>
+                          <td>BSCS</td>
+                          <td>Sample</td>
+                          <td>85/100</td>
+                          <td>
+                            <div class="badge badge-success p-2">Passed</div>
+                          </td>
+                        </tr>
+                        <tr>
+                          <td>
+                            2
+                          </td>
+                          <td>Lorence Lactud</td>
+                          <td>BSIT</td>
+                          <td>Sample</td>
+                          <td>75/100</td>
+                          <td>
+                            <div class="badge badge-warning p-2">Pending</div>
+                          </td>
+                        </tr>
+                        <tr>
+                          <td>
+                            3
+                          </td>
+                          <td>Grace Cortex</td>
+                          <td>BSIS</td>
+                          <td>Sample</td>
+                          <td>70/100</td>
+                          <td>
+                            <div class="badge badge-danger p-2">Failed</div>
+                          </td>
+                        </tr>
+                        <tr>
+                          <td>
+                            4
+                          </td>
+                          <td>Marvin Caharop</td>
+                          <td>BSCS</td>
+                          <td>Sample</td>
+                          <td>90/100</td>
+                          <td>
+                            <div class="badge badge-success p-2">Passed</div>
+                          </td>
+                        </tr>
+                      </tbody>
+                    </table>
+                  </div>
+                  <div class="d-flex mt-4 flex-wrap">
+                    <p class="text-muted">Showing 1 to 10 of 57 entries</p>
+                    <nav class="ml-auto">
+                      <ul class="pagination separated pagination-info">
+                        <li class="page-item"><a href="#" class="page-link"><i class="icon-arrow-left"></i></a></li>
+                        <li class="page-item active"><a href="#" class="page-link">1</a></li>
+                        <li class="page-item"><a href="#" class="page-link">2</a></li>
+                        <li class="page-item"><a href="#" class="page-link">3</a></li>
+                        <li class="page-item"><a href="#" class="page-link">4</a></li>
+                        <li class="page-item"><a href="#" class="page-link"><i class="icon-arrow-right"></i></a></li>
+                      </ul>
+                    </nav>
+                  </div>
+                </div>
+              </div>
+            </div>
+          
+        </div>
+        <!-- content-wrapper ends -->
+        <!-- partial:../../partials/_footer.html -->
+        <footer class="footer">
+          <div class="d-sm-flex justify-content-center justify-content-sm-between">
+            <span class="text-muted d-block text-center text-sm-left d-sm-inline-block">Copyright BSCS4B Group 3</span>
+          </div>
+        </footer>
+      </div>
+      <!-- main-panel ends -->
+    </div>
+    <!-- page-body-wrapper ends -->
+    </div>
+    <!-- container-scroller -->
+    <!-- plugins:js -->
+    <script src="../vendors/js/vendor.bundle.base.js"></script>
+    <!-- endinject -->
+    <!-- Plugin js for this page -->
+    <script src="../vendors/chart.js/Chart.min.js"></script>
+    <script src="../vendors/moment/moment.min.js"></script>
+    <script src="../vendors/daterangepicker/daterangepicker.js"></script>
+    <script src="../vendors/chartist/chartist.min.js"></script>
+    <!-- End plugin js for this page -->
+    <!-- inject:js -->
+    <script src="../js/off-canvas.js"></script>
+    <script src="../js/misc.js"></script>
+    <!-- endinject -->
+    <!-- Custom js for this page -->
+    <script src="../js/dashboard.js"></script>
+    <!-- End custom js for this page -->
+  </body>
+</html>
