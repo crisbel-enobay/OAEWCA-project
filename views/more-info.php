ae42d313ca3e767a24beedbf937be608f8f43cb2
Crisbel-Enobay <91605851+Crisbel-Enobay@users.noreply.github.com>
2023-01-05 14:50:42 +0800

    Update more-info.php

--- views/more-info.php
+++ views/more-info.php
@@ -1,3 +1,7 @@
+<?php
+include '../file/logout-function.php';
+?>
+
 <!DOCTYPE html>
 <html lang="en">
   <head>
@@ -42,7 +46,7 @@
                   <p class="mb-1 mt-3">Student user</p>
                   <p class="font-weight-light text-muted mb-0">student@gmail.com</p>
                 </div>
-                <a class="dropdown-item"><i class="dropdown-item-icon icon-power text-primary"></i>Sign Out</a>
+                <a href="?log=out" class="dropdown-item"><i class="dropdown-item-icon icon-power text-primary"></i>Sign Out</a>
               </div>
             </li>
           </ul>
