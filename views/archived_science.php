a4b4ec89b826bdc121de301eb6775adbb0f9c661
Zaja1520 <74290880+Zaja1520@users.noreply.github.com>
2023-01-13 15:51:54 +0800

    add session fullname in admin pages and change the session checker include

--- views/archived_science.php
+++ views/archived_science.php
@@ -1,6 +1,7 @@
 <?php
     include '../forms/adminQueries.php';
     include '../file/logout-function.php';
+    include "admin-checker.php";
 ?>
 <!DOCTYPE html>
 <html lang="en">
@@ -36,14 +37,14 @@
           <a class="navbar-brand brand-logo-mini" href="../views/admin.php"><img src="../assets/img/OAEWCA-LOGO copy.png" alt="logo" /></a>
         </div>
         <div class="navbar-menu-wrapper d-flex align-items-center flex-grow-1">
-          <h5 class="mb-0 font-weight-medium d-none d-lg-flex">Welcome Admin!</h5>
+          <h5 class="mb-0 font-weight-medium d-none d-lg-flex">Welcome <?php echo ($_SESSION['fullname']); ?>!</h5>
           <ul class="navbar-nav navbar-nav-right ml-auto">
             <li class="nav-item dropdown d-none d-xl-inline-flex user-dropdown">
               <a class="nav-link dropdown-toggle" id="UserDropdown" href="#" data-toggle="dropdown" aria-expanded="false">
-               <span class="font-weight-normal"> Admin admin </span></a>
+               <span class="font-weight-normal"><?php echo ($_SESSION['fullname']); ?></span></a>
               <div class="dropdown-menu dropdown-menu-right navbar-dropdown" aria-labelledby="UserDropdown">
                 <div class="dropdown-header text-center">
-                  <p class="mb-1 mt-3">Admin admin</p>
+                  <p class="mb-1 mt-3"><?php echo ($_SESSION['fullname']); ?></p>
                   <p class="font-weight-light text-muted mb-0">admin@gmail.com</p>
                 </div>
                 <a href="?log=out" class="dropdown-item"><i class="dropdown-item-icon icon-power text-primary"></i>Sign Out</a>
@@ -72,7 +73,7 @@
             <li class="nav-item nav-profile">
               <a href="#" class="nav-link">
                 <div class="text-wrapper">
-                  <p class="profile-name">Admin admin</p>
+                  <p class="profile-name"><?php echo ($_SESSION['fullname']); ?></p>
                   <p class="designation">Administrator</p>
                 </div>
                 <div class="icon-container">
