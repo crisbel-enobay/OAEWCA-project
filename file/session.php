5123346c50e2e3466e69d48ba7abbca64babe435
Zaja1520 <74290880+Zaja1520@users.noreply.github.com>
2023-01-13 19:16:46 +0800

    fix going to admin or student page if not logged in

--- file/session.php
+++ file/session.php
@@ -1,12 +1,15 @@
 <?php
+   if(session_status() !== PHP_SESSION_ACTIVE) 
+   {
     session_start();
+   }
     if (isset($_SESSION['valid'])){
     if ($_SESSION['valid'] == true){
         if ($_SESSION['type'] == 'admin'){
-             echo "<script> window.location = '../views/admin.php' </script>";
+             echo "<script> window.location = './views/admin.php' </script>";
         }
         else if ($_SESSION['type'] == 'student'){
-             echo "<script> window.location = '../views/user-dashboard.php' </script>";
+             echo "<script> window.location = './views/user-dashboard.php' </script>";
         }
     }
 }
