d25a7c85843f8faca38de790698faba7e65111ba
Zaja1520 <74290880+Zaja1520@users.noreply.github.com>
2023-02-13 10:51:56 +0800

    fixed updating password with old password

--- forms/update-password.php
+++ forms/update-password.php
@@ -4,14 +4,20 @@ if(isset($_POST['password']) && $_POST['reset_link_token'] && $_POST['email'] &&
 $conn = mysqli_connect('localhost', "root", "", "project");
 $emailId = $_POST['email'];
 $token = $_POST['reset_link_token'];
-$password = password_hash($_POST['password'], PASSWORD_DEFAULT);
+$new_password = $_POST['password'];
 $query = mysqli_query($conn,"SELECT * FROM `users` WHERE `reset_link_token`='".$token."' and `email`='".$emailId."'");
-$row = mysqli_num_rows($query);
+$row = mysqli_fetch_assoc($query);
 if($row){
-mysqli_query($conn,"UPDATE users set  password='" . $password . "', reset_link_token='" . NULL . "' ,expiry_reset_link_token='" . NULL . "' WHERE email='" . $emailId . "'");
+  $old_password = $row["password"];
+  if (password_verify($new_password, $old_password)) {
+    $password_reset_status = "<p>Error: New password should not be the same as the old password.</p>";
+  } else {
+    $password = password_hash($new_password, PASSWORD_DEFAULT);
+    mysqli_query($conn,"UPDATE users set  password='" . $password . "', reset_link_token='" . NULL . "' ,expiry_reset_link_token='" . NULL . "' WHERE email='" . $emailId . "'");
     $password_reset_status = "<p>Congratulations! Your password has been updated successfully.</p>";
+  }
 }else{
-     $password_reset_status = "<p>Congratulations! Your password has been updated successfully.</p>";
+     $password_reset_status = "<p>Error: Invalid email or reset link token.</p>";
 }
 }
 ?>
