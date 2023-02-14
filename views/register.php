a06a0fff6e7a5ffc7f945fec6cbc5e7e391f0ace
Zaja1520 <74290880+Zaja1520@users.noreply.github.com>
2023-02-14 13:13:20 +0800

    fix wrong redirection if logged in

--- views/register.php
+++ views/register.php
@@ -2,7 +2,23 @@
 <html lang="en">
   
 <?php
-    include '..\file\session.php';
+
+   if(session_status() !== PHP_SESSION_ACTIVE) 
+   {
+    session_start();
+   }
+    if (isset($_SESSION['valid'])){
+    if ($_SESSION['valid'] == true){
+        if ($_SESSION['type'] == 'admin'){
+             echo "<script> window.location = 'admin.php' </script>";
+        }
+        else if ($_SESSION['type'] == 'student'){
+             echo "<script> window.location = 'user-dashboard.php' </script>";
+        }
+    }
+}
+
+
     //Import PHPMailer classes into the global namespace
     //These must be at the top of your script, not inside a function
     use PHPMailer\PHPMailer\PHPMailer;
