507e47a821c744720f7513fe5e4fef35a6499bd9
Zaja1520 <74290880+Zaja1520@users.noreply.github.com>
2023-02-15 16:29:26 +0800

    update insert-key queries

--- forms/insert-key.php
+++ forms/insert-key.php
@@ -3,44 +3,44 @@
  {
   session_start();
  }
-
-                                  if ($_SERVER["REQUEST_METHOD"] == "POST") {
-                                    // Connect to the database
-                                    $servername = "localhost";
-                                    $username = "root";
-                                    $password = "";
-                                    $dbname = "project";
-
-                                    // Create connection
-                                    $conn = new mysqli($servername, $username, $password, $dbname);
-                                    // Check connection
-                                    if ($conn->connect_error) {
-                                      die("Connection failed: " . $conn->connect_error);
-                                    }
-
-                                    $key = $_POST["inputkey"];
-                                    $email = ($_SESSION['email']);
-
-                                    // Check if the key and email exist in the database
-                                    $sql = "SELECT * FROM generated_codes WHERE exam_key='$key' AND email='$email'";
-                                    $result = $conn->query($sql);
-                                    if ($result->num_rows > 0) {
-                                      // Key and email exist, check if the key is expired
-                                      $row = $result->fetch_assoc();
-                                      $expired_date = $row["exam_date"];
-                                      if (time() < strtotime($expired_date)) {
-                                        // Key is expired
-                                        echo "Your key is expired.";
-                                      } else {
-                                        // Key is valid, redirect to other page
-                                        echo "valid";
-                                      }
-                                    } else {
-                                      // Key and email do not exist
-                                      echo "Invalid key";
-                                    }
-                                  
-                                    mysqli_free_result($result);
-                                     
+  // Create connection
+                                  // Create connection
+                                  $conn = new mysqli('localhost', 'root', '', 'project');
+                                  // Check connection
+                                  if ($conn->connect_error) {
+                                    die("Connection failed: " . $conn->connect_error);
                                   }
+  
+                                  $key = $_POST["inputkey"];
+                                  $email = ($_SESSION['email']);
+  
+                                  // Check if the key and email exist in the database
+                                  $sql = "SELECT * FROM generated_codes WHERE exam_key='$key' AND email='$email'";
+                                  $result = $conn->query($sql);
+                                  if ($result->num_rows > 0) {
+                                    // Key and email exist, check if the key is expired
+                                    $row = $result->fetch_assoc();
+                                    $status = $row['status'];
+                                    $exam_date = $row["exam_date"];
+                                    if (date('Y-m-d') > date('Y-m-d', strtotime($exam_date))) {
+                                      // Key is expired
+                                      echo "Your key is expired.";}
+                                      else{
+                                      // Key is valid, check if the status is approved
+                                      if ($status == "approved") {
+                                        // Status is approved
+                                        echo "Your key is valid this date and has been approved. you can take the exam";
+                                      } else {
+                                        // Status is not approved
+                                        echo "Your key is valid but it has not been approved yet.";
+                                      }
+                                    } 
+                                  } else {
+                                    // Key and email do not exist
+                                    echo "Invalid key";
+                                  }
+                                
+                                  mysqli_free_result($result);
+                                   
+                                
                                   ?>
