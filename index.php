6b7924d10bf674e3bca70cbef8744375749bc87f
Crisbel-Enobay <91605851+Crisbel-Enobay@users.noreply.github.com>
2023-05-07 22:41:47 +0800

    removed bootstrapmade

--- index.php
+++ index.php
@@ -428,7 +428,6 @@ include "file/session.php";
         <!-- You can delete the links only if you purchased the pro version. -->
         <!-- Licensing information: https://bootstrapmade.com/license/ -->
         <!-- Purchase the pro version with working PHP/AJAX contact form: https://bootstrapmade.com/ninestars-free-bootstrap-3-theme-for-creative/ -->
-        Designed by <a href="https://bootstrapmade.com/">BootstrapMade</a>
       </div>
     </div>
   </footer><!-- End Footer -->
